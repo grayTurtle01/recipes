f534788d3666f44ff810cadaf2a5effbc22913a5
grayTurtle01 <the.correo@protonmail.com>
2021-12-16 13:29:29 -0600

    sum prices products

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app.models import User, Recipe, DayMenu
+from app.models import User, Recipe, DayMenu, Product
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Recipe)
 admin.site.register(DayMenu)
+admin.site.register(Product)
--- app/models.py
+++ app/models.py
@@ -30,3 +30,6 @@ class DayMenu(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} | {self.price} "
